3d1e7d8ae0e425fb4e988bc7057ba234072c5298
andrejskalnins <andrejs.kalnins@gmail.com>
2021-10-20 21:39:59 +0100

    Exercise 2: done

--- exercises/exercise-2/exercise-2.js
+++ exercises/exercise-2/exercise-2.js
@@ -70,3 +70,36 @@ let hogwarts = [
     occupation: "Teacher",
   },
 ];
+
+// Task 1
+function getGryffindorStudentNames(array) {
+  let [...houseGryffindor] = array.filter(
+    (item) => item.house === "Gryffindor"
+  );
+  houseGryffindor.forEach(({ firstName, lastName }) => {
+    console.log(`${firstName} ${lastName}`);
+  });
+}
+getGryffindorStudentNames(hogwarts);
+
+// Task 2
+// Question: Are either of these correct use of array destructuring as per exercise instructions?
+
+// Version 1
+function professorWhoOwnsAPet([{ occupation, pet, firstName, lastName }]) {
+  if (occupation === "Teacher" && pet !== null) {
+    console.log(firstName + lastName);
+  }
+}
+professorWhoOwnsAPet(hogwarts);
+
+// Version 2
+
+function findProfessorWhoOwnsAPet(array) {
+  const [{ ...professorWithAPet }] = array.filter(
+    (person) => person.occupation === "Teacher" && person.pet !== null
+  );
+  console.log(professorWithAPet.firstName, professorWithAPet.lastName);
+}
+
+findProfessorWhoOwnsAPet(hogwarts);
